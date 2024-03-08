8c7014cdebc8d1ab5ce74514e623b8eb4eeb2752
Костянтин Іванович Дрімуш <kostiantyn.i.drimush@ukd.edu.ua>
2024-03-08 13:54:53 +0200

    Create file

--- text.py
+++ text.py
@@ -1,1 +1,25 @@
-print("Hello world!")
+class Eren:
+    def __init__(self):
+        self.nameeren = "Eren Yeager"
+        self.ageeren = 19
+        self.titaneren = "Attack titan, Founder titan, War hammer titan"
+    def getinfo(self):
+        a = input("Персонаж: ")
+        if a.lower() == "eren":
+            print(f"Name: {self.nameeren} \nAge: {self.ageeren} \nTitan: {self.titaneren} ")
+
+class Armin:
+
+    def __init__(self):
+        self.namearmin = "Armin Arlert"
+        self.agearmin = 19
+        self.titanarmin = "Collosal titan"
+    def info(self):
+        b = input("Персонаж: ")
+        if b.lower() == "armin":
+            print(f"Name: {self.namearmin} \nAge: {self.agearmin} \nTitan: {self.titanarmin} ")
+sigma = Eren()
+sigma.getinfo()
+
+sigma = Armin()
+sigma.info()
