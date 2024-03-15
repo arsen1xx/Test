531dda50762a1c240449f7ba59a9ecc69619664e
Костянтин Іванович Дрімуш <kostiantyn.i.drimush@ukd.edu.ua>
2024-03-15 13:20:12 +0200

    Names

--- text.py
+++ text.py
@@ -1,25 +1,83 @@
+def inform():
+    menu = (True)
+    while menu == True:
+        name = str(input("Введіть ім'я персонажа: "))
+        if name == "Eren" or name == "eren":
+            erens = Eren()
+            print(f"Name: {erens.nameeren} \nAge: {erens.ageeren} \nTitan: {erens.titaneren} ")
+        elif name == "Armin" or name == "armin":
+            armins = Armin()
+            print(f"Name: {armins.namearmin} \nAge: {armins.agearmin} \nTitan: {armins.titanarmin} ")
+        elif name == "Eren Kruger" or name == "erenk kruger":
+            erenks = Erenk()
+            print(f"Name: {erenks.nameerenk} \nAge: {erenks.ageerenk} \nTitan: {erenks.titanerenk} ")
+
+
 class Eren:
-    def __init__(self):
-        self.nameeren = "Eren Yeager"
-        self.ageeren = 19
-        self.titaneren = "Attack titan, Founder titan, War hammer titan"
-    def getinfo(self):
-        a = input("Персонаж: ")
-        if a.lower() == "eren":
-            print(f"Name: {self.nameeren} \nAge: {self.ageeren} \nTitan: {self.titaneren} ")
-
+    nameeren = "Eren Yeager"
+    ageeren = 19
+    titaneren = "Attack Titan, Founding Titan, War Hammer Titan"
 class Armin:
+    namearmin = "Armin Arlert"
+    agearmin = 19
+    titanarmin = "Colossal Titan"
+class Erenk:
+    nameerenk = "Eren Kruger"
+    ageerenk = 55
+    titanerenk = "Attack Titan"
+class Grisha:
+    namegrisha = "Grisha Jaeger"
+    agegrisha = 39
+    titangrisha = "Attack Titan, Founding Titan"
+class Ymir:
+    nameymir = "Ymir Fritz"
+    ageymir = 23
+    titanymir = "Founding Titan"
+class Frieda:
+    namefrieda = "Frieda Reiss"
+    agedina = 18
+    titanfrieda = "Attack Titan, Founding Titan"
+class Lara:
+    namelara = "Lara Tybur"
+    agelara = 29
+    titanlara = "War Hammer Titan"
+class Zeke:
+    namezeke = "Zeke Yaeger"
+    agezeke = 15
+    titanzeke = "Beast Titan"
+class Marcel:
+    namemarcel = "Marcel Galliard"
+    agegalliard = 12
+    titanmarcel = "Jaw Titan"
+class Ymir:
+    nameymir = "Ymir"
+    ageymir = 17
+    titanymir = "Jaw Titan"
+class Porco:
+    nameporco = "Porco Galliard"
+    ageporco = 19
+    titanporco = "Jaw Titan"
+class Falco:
+    namefalco = "Falco Grice"
+    agefalco = 15
+    titanfalco = "Jaw Titan"
+class Bertholdt:
+    nameber = "Bertholdt Hoover"
+    ageber = 17
+    titanber = "Colossal Titan"
+class Reiner:
+    namereiner = "Reiner Braun"
+    agereiner = 21
+    titanreiner = "Armored Titan"
+class Annie:
+    nameannie = "Annie Leonhart"
+    ageannie = 16
+    titanannie = "Female Titan"
+class Pieck:
+    namepieck = "Pieck Finger"
+    agepieck = 23
+    titanpieck = "Cart Titan"
 
-    def __init__(self):
-        self.namearmin = "Armin Arlert"
-        self.agearmin = 19
-        self.titanarmin = "Collosal titan"
-    def info(self):
-        b = input("Персонаж: ")
-        if b.lower() == "armin":
-            print(f"Name: {self.namearmin} \nAge: {self.agearmin} \nTitan: {self.titanarmin} ")
-sigma = Eren()
-sigma.getinfo()
 
-sigma = Armin()
-sigma.info()
+
+inform()
